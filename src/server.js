e05b7a3768e5ea8842b7e357f99a55f25d6fe884
thekenyanguy <jorammwalepe@gmail.com>
2025-08-13 21:33:30 +0300

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,31 +1,37 @@
-//const express = require('express');
-import express from 'express';
-import dotenv from 'dotenv';
-import { initDB } from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-import transactionsRoute from './routes/transactionsRoute.js';
-
+import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
-// Initialize express app
+
 const app = express();
+
 if (process.env.NODE_ENV === "production") job.start();
-// Middleware to handle CORS 
+
+// middleware
 app.use(rateLimiter);
 app.use(express.json());
 
+// our custom simple middleware
+// app.use((req, res, next) => {
+//   console.log("Hey we hit a req, the method is", req.method);
+//   next();
+// });
+
 const PORT = process.env.PORT || 5001;
 
-
-
-
-app. use('/api/transactions', transactionsRoute);
-
-// Start the server and initialize the database
-initDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Server is running on PORT:", PORT);
-    });
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
+});
