5e3d85e50e70850d7d1bef717b1f3efd0ee1bc7b
Mbati <84579912+thekenyanguy@users.noreply.github.com>
2025-08-14 23:23:27 +0300

    Update upstash.js

--- src/config/upstash.js
+++ src/config/upstash.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 // Creates a Redis client using Upstash credentials from environment variables
 const ratelimiter = new Ratelimit({
 redis: Redis.fromEnv(),
-limiter: Ratelimit.slidingWindow(5, "60 s"), // 4 requests per minute
+limiter: Ratelimit.slidingWindow(1006, "60 s"), // 4 requests per minute
 });
 
 export default ratelimit;
