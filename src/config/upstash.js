29e133c43913820b904afa9672603457066dbb46
thekenyanguy <jorammwalepe@gmail.com>
2025-08-14 23:05:59 +0300

    Updates

--- src/config/upstash.js
+++ src/config/upstash.js
@@ -1,15 +1,12 @@
-import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
 
 import "dotenv/config";
 
 // Creates a Redis client using Upstash credentials from environment variables
 const ratelimiter = new Ratelimit({
 redis: Redis.fromEnv(),
-limiter: Ratelimit.slidingWindow(5000, "60 s"), // 4 requests per minute
+limiter: Ratelimit.slidingWindow(5, "60 s"), // 4 requests per minute
 });
 
-export default ratelimiter;
-
-
-
+export default ratelimit;
--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,21 +1,20 @@
 import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
-     
-    try {
-        // Check rate limit for the user based on their IP address
-        const {success} = await ratelimit.limit("my-rate-limit")
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
 
-        if (!success) {
-            return res.status(429).json({ message: "Too many requests, please try again later.", 
-    })
-  }
-        // If the request is within the limit, proceed to the next middleware
-            next(); // Proceed to the next middleware or route handler
-    } catch (error) {
-        console.log("Rate limit error:", error);
-        next(error); // Call next with the error to handle it in the error handler  
+    if (!success) {
+      return res.status(429).json({
+        message: "Too many requests, please try again later.",
+      });
     }
+
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
 };
 
 export default rateLimiter;
